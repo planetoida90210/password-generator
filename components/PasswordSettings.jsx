828c786f79e96bc643be64aa145155c771d21f3d
Michał Bryk <galeriakoloru90210@gmail.com>
2022-10-09 22:53:14 +0200

    refactor range slider to component

--- components/PasswordSettings.jsx
+++ components/PasswordSettings.jsx
@@ -1,17 +1,11 @@
 import React,{ useState } from 'react';
+import RangeSlider from './RangeSlider';
 
 
 const PasswordSettings = () => {
     
   const [defaultCharValue, setRangeValue] = useState(0);
 
-  const passwordLength = 3
-
-  const bgSlider = (passwordLength * 100) / 20
-
-  const setSliderBackground = () => (
-    `linear-gradient(90deg, #A4FFAF ${bgSlider}%, #18171F ${bgSlider}%)`
-  )
 
   return (
     <div className="w-[343px] h-[423px] md:w-[540px] md:h-[528px] mt-4 md:mt-8 bg-[#24232C] rounded-md">
@@ -19,16 +13,7 @@ const PasswordSettings = () => {
         <h3 className="text-[#E6E5EA] text-[18px]">Character Length</h3>
         <p className="text-[24px] md:text-[32px] leading-4 md:leading-8 text-[#A4FFAF]">{defaultCharValue}</p>
      </div>
-     <div className="pt-[23px] md:pt-[35px] w-full flex justify-center">
-        <input
-        min="0"
-        max="20"
-        type="range"
-        value={passwordLength}
-        style={{background: setSliderBackground()}}
-        className="w-[311px] md:w-[476px] h-[8px] appearance-none" />
-        
-     </div>
+     <RangeSlider />
     </div>
   )
 }
---
+++ components/RangeSlider.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const RangeSlider = () => {
+  
+    const passwordLength = 3
+
+    const bgSlider = (passwordLength * 100) / 20
+  
+    const setSliderBackground = () => (
+      `linear-gradient(90deg, #A4FFAF ${bgSlider}%, #18171F ${bgSlider}%)`
+    )
+
+  return (
+    <div className="pt-[23px] md:pt-[35px] w-full flex justify-center">
+        <input
+        min="0"
+        max="20"
+        type="range"
+        onChange={(e) => ""}
+        value={passwordLength}
+        style={{background: setSliderBackground()}}
+        className="w-[311px] md:w-[476px] h-[8px] appearance-none" />
+        
+     </div>
+  )
+}
+
+export default RangeSlider
