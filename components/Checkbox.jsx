737c89c3c451d0b5586213e3611842de9f7d5272
Michał Bryk <galeriakoloru90210@gmail.com>
2022-10-10 13:04:00 +0200

    set checked input css

--- components/Checkbox.jsx
+++ components/Checkbox.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Checkbox = () => {
+  const [isChecked,setIsChecked] = useState(false)
   return (
     <div className="flex flex-row-reverse pt-[16px]">
         <div className="text-[#E6E5EA] text-[16px] md:text-[18px] leading-[20px]">
             <label>Include uppercase letters</label>
         </div>
         <div className="pr-[16px]">
-            <input type="checkbox" checked="false" />
+            <input
+             className="flex items-center justify-center" 
+             type="checkbox" 
+             checked={isChecked}
+             onChange={() => setIsChecked((prev) => !prev)}
+              />
         </div>
     </div>
   )
