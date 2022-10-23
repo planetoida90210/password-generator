20b268e0702c42b0d4ed25c5ebe297e052fd8d7e
Michał Bryk <galeriakoloru90210@gmail.com>
2022-10-23 19:46:16 +0200

    create password settings in utils

---
+++ utils/passwordUtils.js
@@ -0,0 +1,31 @@
+const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+const NUMBERS = '0123456789';
+const SYMBOLS = '!@#$%^&*_-+=';
+
+export const generatePassword = ({ length = 8, options }) => {
+    const { upperCase, lowerCase, numbers, symbols } = options;
+  
+    const characterPool = [];
+  
+    if (upperCase) characterPool.push(...UPPERCASE_LETTERS);
+    if (lowerCase) characterPool.push(...LOWERCASE_LETTERS);
+    if (numbers) characterPool.push(...NUMBERS);
+    if (symbols) characterPool.push(...SYMBOLS);
+  
+    return Array.from({ length }, () => characterPool[Math.floor(Math.random() * characterPool.length)]).join('');
+  };
+  
+  export const measureStrength = ({ length, options }) => {
+    const { upperCase, lowerCase, numbers, symbols } = options;
+  
+    const numberOfChecked = (Number(upperCase) + Number(lowerCase) + Number(numbers) + Number(symbols));
+  
+    if (length < 0 || numberOfChecked === 0) return 0;
+    if (length < 8) return 1;
+    if (length < 12 && numberOfChecked <= 2) return 2;
+    if ((length < 12 && numberOfChecked > 2) || (length >= 12 && numberOfChecked <= 2)) return 3;
+    if (length >= 12 && numberOfChecked > 2) return 4;
+  
+    return 0;
+  };
